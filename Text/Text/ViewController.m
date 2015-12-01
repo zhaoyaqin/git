7b9aea4f8ba6dd6df7e09718a8ea6f9aeaddcd5f
zhaoyaqin <981371052@qq.com>
2015-12-01 15:33:56 +0800

    text

---
+++ Text/Text/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Text
+//
+//  Created by 赵亚琴 on 15/12/1.
+//  Copyright © 2015年 赵亚琴. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Text/Text/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Text
+//
+//  Created by 赵亚琴 on 15/12/1.
+//  Copyright © 2015年 赵亚琴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
