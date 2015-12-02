b05464d0b319196788c57c988d108b58da9ebadf
zhaoyaqin <981371052@qq.com>
2015-12-02 09:36:23 +0800

    add two

---
+++ TextTwo/TextTwo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TextTwo
+//
+//  Created by 赵亚琴 on 15/12/2.
+//  Copyright © 2015年 赵亚琴. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
